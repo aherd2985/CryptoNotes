383d13b003ded1033730da4b9cb51dd43e3d0fdb
aherd2985 <andrew.herd@okstate.edu>
2020-11-15 17:42:58 -0600

    Initial upload

---
+++ CryptoNotes/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CryptoNotes.Models;
+
+namespace CryptoNotes.ViewModels
+{
+  public class ItemDetailViewModel : BaseViewModel
+  {
+    public Item Item { get; set; }
+    public ItemDetailViewModel(Item item = null)
+    {
+      Title = item?.Text;
+      Item = item;
+    }
+  }
+}
---
+++ CryptoNotes/Views/ItemsPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Forms;
+using CryptoNotes.Models;
+using CryptoNotes.ViewModels;
+
+namespace CryptoNotes.Views
+{
+  public partial class ItemsPage : ContentPage
+  {
+    ItemsViewModel viewModel;
+
+    public ItemsPage()
+    {
+      InitializeComponent();
+    }
+
+    async void OnItemSelected(object sender, EventArgs args)
+    {
+      var layout = (BindableObject)sender;
+      var item = (Item)layout.BindingContext;
+      await Navigation.PushAsync(new ItemDetailPage(new ItemDetailViewModel(item)));
+    }
+
+    async void AddItem_Clicked(object sender, EventArgs e)
+    {
+      await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
+    }
+
+    protected override void OnAppearing()
+    {
+      base.OnAppearing();
+
+      BindingContext = viewModel = new ItemsViewModel();
+
+      if (viewModel.Items.Count == 0)
+        viewModel.IsBusy = true;
+    }
+  }
+}
